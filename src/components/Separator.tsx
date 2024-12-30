7ee1416a4e0a7593bc4837c77f08c12360b52455
mattchaib <mattchaib@gmail.com>
2024-12-30 11:51:50 +0000

    redeploy

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,6 +1,6 @@
 export function Separator() {
     const hrStyle = {
-      border: '0',
+      border: '0', // No border
       borderTop: '1px solid #ccc', // Thin, light gray line
       margin: '20px 0', // Space around the line
     };
